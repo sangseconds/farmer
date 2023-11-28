fcec27d4fea4b292a8feb4f5262510d1e89a9ce2
sangseconds <82400654+sangseconds@users.noreply.github.com>
2023-11-28 14:40:59 +0700

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,12 +1,12 @@
-# import os
-# import wget
+import os
+import wget
 
 # # data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
 
-# # Download the zipped dataset
-# url = 'https://data.mendeley.com/public-files/datasets/yshdbyj6zy/files/6722d2a1-b669-427e-9c1e-f1360ad485b0/file_downloaded'
-# csv_name = "data_raw.csv"
-# wget.download(url, csv_name)
+# Download the zipped dataset
+url = 'https://data.mendeley.com/public-files/datasets/yshdbyj6zy/files/6722d2a1-b669-427e-9c1e-f1360ad485b0/file_downloaded'
+csv_name = "data_raw.csv"
+wget.download(url, csv_name)
 
 # Unzip it and standardize the .csv filename
 # import zipfile
@@ -20,9 +20,9 @@
 
 # Xuan viet
 # Đọc nội dung từ file first-data
-with open('rawdata_new.csv', 'r') as first_file:
-    data = first_file.read()
+# with open('rawdata_new.csv', 'r') as first_file:
+#     data = first_file.read()
 
-# Tạo một file mới có tên là second-data và ghi nội dung vào đó
-with open('data_raw.csv', 'w') as second_file:
-    second_file.write(data)
+# # Tạo một file mới có tên là second-data và ghi nội dung vào đó
+# with open('data_raw.csv', 'w') as second_file:
+#     second_file.write(data)
